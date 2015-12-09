f1222ea4da2c6f385410cbb7ccd6b867b9717336
Andrew Noyes <anoyes@slideshare.com>
2015-12-08 22:09:35 -0800

    Added documentation to main class file

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ module.exports = GlobalExportWriter;
 
 /**
  * Broccoli plugin that adds ES6 export statements to files.
+ * @param {string|object} inputTree The tree that contains the file that exports will be added to
+ * @param {string} fileName Name of the file in the input tree to add exports to
+ * @param {object} [options={}] Configuration options for export writer
+ * @param {string} [options.moduleType=es2015] Output module type: es2015, amd, or common
+ * @param {string} [options.defaultExport] The name of the default export for the file
+ * @param {string[]} [options.exports=[]] List of named exports
  */
-function GlobalExportWriter(inputNodes, fileName, options) {
+function GlobalExportWriter(inputTree, fileName, options) {
   if (!(this instanceof GlobalExportWriter)) {
-    return new GlobalExportWriter(inputNodes, fileName, options);
+    return new GlobalExportWriter(inputTree, fileName, options);
   }
 
   options = defaults(options || {}, {
@@ -25,23 +31,25 @@ function GlobalExportWriter(inputNodes, fileName, options) {
     exports: []
   });
 
-  if (Array.isArray(inputNodes)) {
+  if (Array.isArray(inputTree)) {
     throw new TypeError('Does not support more than one input tree');
   }
 
-  BroccoliPlugin.call(this, [inputNodes], {
+  BroccoliPlugin.call(this, [inputTree], {
     annotations: options.annotations
   });
 
   this.fileName = fileName;
   this.moduleType = options.moduleType;
   this.exporter = new GlobalExporter(options.defaultExport, options.exports);
-
   this.shouldTranspile = includes(['amd', 'common'], this.moduleType);
 }
 
 util.inherits(GlobalExportWriter, BroccoliPlugin);
 
+/**
+ * Adds exports to a source code file.
+ */
 GlobalExportWriter.prototype.build = function() {
   var inputPath = first(this.inputPaths),
       srcPath = path.join(inputPath, this.fileName),
@@ -51,13 +59,17 @@ GlobalExportWriter.prototype.build = function() {
   sourceCode = this.exporter.processSourceCode(sourceCode);
 
   if (this.shouldTranspile) {
-    sourceCode = this.transpile(sourceCode);
+    sourceCode = this._transpile(sourceCode);
   }
 
   fs.writeFileSync(destPath, sourceCode);
 };
 
-GlobalExportWriter.prototype.transpile = function(sourceCode) {
+/**
+ * Transpiles ES6 modules to a different type.
+ * @private
+ */
+GlobalExportWriter.prototype._transpile = function(sourceCode) {
   var transpiled = babel.transform(sourceCode, {
     whitelist: ['es6.modules'],
     modules: this.moduleType
